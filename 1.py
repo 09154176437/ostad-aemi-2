a3e717d3cb6658fa83629f27137896b2175d4817
09154176437 <88371981+09154176437@users.noreply.github.com>
2021-08-07 13:19:47 +0430

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,98 @@
+# name = input()
+# family = input()
+# sum =0
+# for i in range(10):
+#     score =  float(input())
+#     sum = sum + score
+
+# average = sum / 10
+
+# print('average:',average)
+
+
+
+
+
+# n = int(input())
+# for i in range (n):
+#     print('helo')
+# print('goodbye')
+
+
+
+
+# fruits = ['apple','banana','orange','pyaz'] 
+# print(fruits[2])
+
+# fruits[1] = 'moz'
+
+# print(fruits)
+
+
+# names = ['amir','naser','mostafa','reza']
+
+# for i in range(len(names)):
+#     if len(names[i]) == 5:
+#       print(names[i])
+
+
+# names = ['amir','naser','mostafa','reza']
+
+# print(names)
+
+# names.append('mohamad')
+# names.append('sagad')
+# names.insert(2,'ahmad')
+
+# print(names)
+
+
+
+# print('enter your username')
+# username = input()
+# print('enter your password')
+# password = input()
+
+# while username != 'admin' or password != '1234':
+#     print('incurrect username or password')
+
+#     print('enter your username')
+#     username = input()
+#     print('enter your password')
+#     password = input()
+# print('user logined')
+
+# import random
+
+# computer_number = random.randint(0.20)
+
+# user_number = int(input())
+
+# if computer_number == user_number:
+#         print('barikala')
+
+# elif computer_number > user_number:
+#         print('boro bala')
+
+# elif computer_number < user_number:
+#         print('boro paeen')
+     
+
+# import random
+
+# computer_number = random.randint(0,20)
+
+# user_number = -1
+
+# while computer_number != user_number:
+    
+#     user_number = int(input())
+
+#     if computer_number == user_number:
+#         print('barikala')
+
+#     elif computer_number > user_number:
+#         print('boro bala')
+        
+#     elif computer_number < user_number:
+#         print('boro paeen')
